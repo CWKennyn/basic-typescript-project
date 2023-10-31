7f37abb67756d9d0725584ad6f6f8e547914d7e6
Kenny Nguyen <Kenny.Nguyen@coxautoinc.com>
2023-10-31 09:40:43 +0000

    Second unit test passing via constant -> conditional

--- src/who-likes-it/index.ts
+++ src/who-likes-it/index.ts
@@ -1,4 +1,10 @@
 export const likes = (a : string[]) : string => {
-    // TODO
-    return 'no one likes this';
+    if (a.length === 0) {
+        return 'no one likes this';
+    }
+    if (a.length === 1) {
+        return `${a.toString()} likes this`
+    }
+    return a.toString();
+
 }
