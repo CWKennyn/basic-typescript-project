5b081efaf3ee50a243d422fe8c8b723b6d3de18a
Kenny Nguyen <Kenny.Nguyen@coxautoinc.com>
2023-10-31 09:56:34 +0000

    Third unit test passing via TDD

--- src/who-likes-it/index.ts
+++ src/who-likes-it/index.ts
@@ -1,9 +1,10 @@
 export const likes = (a : string[]) : string => {
     if (a.length === 0) {
         return 'no one likes this';
-    }
-    if (a.length === 1) {
-        return `${a.toString()} likes this`
+    } else if (a.length === 1) {
+        return `${a[0]} likes this`;
+    } else if (a.length === 2) {
+        return `${a[0]} and ${a[1]} like this`;
     }
     return a.toString();
 
