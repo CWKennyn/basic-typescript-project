07421c258242400888b56de851fea3bcd9c713ee
Kenny Nguyen <Kenny.Nguyen@coxautoinc.com>
2023-11-02 10:06:25 +0000

    Got the second test passing via TDD

--- src/persistent-bugger/index.ts
+++ src/persistent-bugger/index.ts
@@ -1,3 +1,12 @@
 export function persistence(num: number): number {
-    return 3;
+    const numToSplitStringArray = String(num).split('');
+    let splitStringArrayToNumArray = numToSplitStringArray.map(Number);
+    let count = 3;
+
+    if (splitStringArrayToNumArray.length < 2) {
+        return 0;
+    }
+
+    return count;
 }
+
