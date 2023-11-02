4107e0939063bcccc901fe878f214c76c18a5163
Kenny Nguyen <Kenny.Nguyen@coxautoinc.com>
2023-11-02 10:10:22 +0000

    Solution for this kata with all tests passing

--- src/persistent-bugger/index.ts
+++ src/persistent-bugger/index.ts
@@ -1,10 +1,20 @@
 export function persistence(num: number): number {
     const numToSplitStringArray = String(num).split('');
     let splitStringArrayToNumArray = numToSplitStringArray.map(Number);
-    let count = 3;
+    let count = 0;
 
     if (splitStringArrayToNumArray.length < 2) {
         return 0;
+    }
+
+    while (splitStringArrayToNumArray.length > 1) {
+        let product = 1;
+        for (const num of splitStringArrayToNumArray) {
+            product *= num;
+        }
+
+        splitStringArrayToNumArray = Array.from(product.toString(), Number);
+        count++;
     }
 
     return count;
