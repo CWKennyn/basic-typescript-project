1521d8b51c1729e7813c93fa5c0ddc650c427073
Kenny Nguyen <Kenny.Nguyen@coxautoinc.com>
2023-11-06 09:57:35 +0000

    Getting rid of unnecessary new lines

--- src/is-he-gonna-survive/index.ts
+++ src/is-he-gonna-survive/index.ts
@@ -1,6 +1,4 @@
 export function hero(bullets: number, dragons: number): boolean {
     const calculateTwoBulletsPerDragonToOneBullet = bullets / 2;
     return calculateTwoBulletsPerDragonToOneBullet >= dragons;
-
-
 }
